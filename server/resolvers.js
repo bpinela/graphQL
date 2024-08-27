9b0c30a58096bddb2473ba13780027055817baf6
bpinela <bruno.pinelamarques@gmail.com>
2024-08-27 20:55:55 -0300

    Error handler

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,12 +1,26 @@
 import { getJobs, getJob, getJobsByCompany } from './db/jobs.js';
 import { getCompany } from './db/companies.js';
+import { GraphQLError } from 'graphql';
 
 
 export const resolvers = {
   Query: {
     jobs: () => getJobs(),
-    job: (_root, {id}) => getJob(id),
-    company: (_root, {id}) => getCompany(id)
+    job: async (_root, {id}) => { 
+      const job = await getJob(id)
+      if (!job) {
+        throw notFoundError(`No job found with id ${id}`)
+      }
+      return job
+    },
+    company: async (_root, {id}) => {
+      const company = await getCompany(id)
+      if (!company) {
+        throw notFoundError(`No company found with id ${id}`);
+      }
+
+      return company
+    }
   },
 
   Company: {
@@ -18,6 +32,11 @@ export const resolvers = {
     date: (job) => toISODate(job.createdAt)
   },
 };
+
+const notFoundError = (message) => {
+  return new GraphQLError(message, { extensions: { code: "NOT_FOUND" }})
+        
+}
 
 const toISODate = (value) => {
   return value.slice(0, 'yyyy-mm-dd'.length)
